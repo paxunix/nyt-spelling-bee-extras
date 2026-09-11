3d00bbf8d2341ef6ea91f634f8a0d5bfeb11f06d
Shawn Halpenny <paxunix@gmail.com>
2026-09-11 12:40:40 -0700

    refactor(script): use NYT puzzle data for hints

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 nyt-spelling-bee-extras
 =======================
 
-Greasemonkey script that puts some information from the Today's Hints page
-into the Spelling Bee page.
+Greasemonkey script that puts extra puzzle information into the Spelling
+Bee page.
 
 It displays a grid next to the hive that shows the total count of words
 needed, how many pangrams, and the count of each letter pair.  The
--- nyt-spelling-bee-extras.user.js
+++ nyt-spelling-bee-extras.user.js
@@ -8,10 +8,9 @@
 // @updateURL   https://raw.githubusercontent.com/paxunix/nyt-spelling-bee-extras/main/nyt-spelling-bee-extras.user.js
 // @require     https://cdn.jsdelivr.net/gh/paxunix/WaitForElements/WaitForElements.min.js
 // @grant       GM.addStyle
-// @grant       GM.xmlHttpRequest
 // @grant       unsafeWindow
 // @grant       GM.registerMenuCommand
-// @version     25
+// @version     26
 // ==/UserScript==
 
 
@@ -24,30 +23,6 @@
 
 const doneBackgroundColor = "#dcffdc";
 const longWordBackgroundColor = "#46f8f9";
-
-
-function getPuzzleISODate()
-{
-    let isoDate = unsafeWindow?.gameData?.today?.printDate;
-
-    if (!isoDate)
-        throw Error("failed to get date for this puzzle");
-
-    return isoDate;
-}
-
-
-function getDateParts(isoDateStr)
-{
-    let [year, month, day] = isoDateStr.split("-");
-    return {year, month, day};
-}
-
-
-function getDateWithDelimiter(dateParts, delim)
-{
-    return [ dateParts.year, dateParts.month.padStart(2, "0"), dateParts.day.padStart(2, "0")].join(delim);
-}
 
 
 function navTo(curIsoDateStr, numDays)
@@ -63,38 +38,6 @@ function navTo(curIsoDateStr, numDays)
 }
 
 
-async function fetcher(opts)
-{
-    return new Promise((res, rej) => {
-        opts.headers = opts.headers || {};
-        opts.headers.Accept = opts.headers.Accept || "*/*";
-        opts.method = opts.method || "GET";
-        opts.onload = response => {
-            if (response.status >= 200 && response.status < 300)
-            {
-                res(response);
-            }
-            else
-            {
-                rej({
-                    status: response.status,
-                    statusText: `${response.status} ${response.statusText} retrieving ${opts.url}`
-                });
-            }
-        };
-        opts.onerror = response => {
-            rej({
-                status: response.status,
-                statusText: `${response.status} ${response.statusText} retrieving ${opts.url}`
-            });
-
-        };
-
-        return GM.xmlHttpRequest(opts);
-    });
-}
-
-
 function isEachLetterUsedOnce(word)
 {
     let o = {};
@@ -107,61 +50,47 @@ function isEachLetterUsedOnce(word)
 }
 
 
-function wordIsPangram(checkWord, letters)
+function getPuzzleData()
 {
-    let needLetters = new Map(letters.map(t => [ t.toLowerCase(), 1 ]));
-    for (let l of checkWord.split(""))
-    {
-        needLetters.delete(l.toLowerCase());
-    }
-
-    return needLetters.size === 0;
+    return unsafeWindow?.gameData?.today ?? {};
 }
 
 
-async function fetchHintInfo(isoPuzzleDateStr, allLetters)
+function requirePuzzleData()
 {
-    let url = new URL(`${isoPuzzleDateStr}/answers`, "https://spellingbeesolver.net/");
+    let puzzleData = getPuzzleData();
 
-    let response = await fetcher({
-        url: url,
-        responseType: "document",
-    });
+    if (!puzzleData.printDate || !Array.isArray(puzzleData.answers) || !Array.isArray(puzzleData.pangrams))
+    {
+        throw Error("failed to get puzzle data");
+    }
 
-    let doc = response.response;
-    let wordStats = {};
+    return puzzleData;
+}
+
+
+function buildHintInfo(puzzleData)
+{
     let twoLetter2Count = {};
 
-    let $allSolutions = Array.from(doc.querySelectorAll('section')).filter(el => el.innerText.search(/All.*Solutions/) !== -1);
-    if ($allSolutions.length === 0)
-        throw Error(`Spelling Bee Solver page format changed: expected "all solutions" section at ${url}`);
-
-    let wordEls = Array.from($allSolutions[0].querySelectorAll(`button:has(span.uppercase)`));
-    if (wordEls.length === 0)
-        throw Error(`Spelling Bee Solver page format changed: expected button:has(span.uppercase) at ${url}`);
-
-    let wordlist = Array.from($allSolutions[0].querySelectorAll(`button:has(span.uppercase)`)).map($el => $el.innerText.replaceAll(/\W+/g, "").toLowerCase());
-    let pangramList = wordlist.filter(w => wordIsPangram(w, allLetters));
-    let perfectPangramList = pangramList.filter(el => isEachLetterUsedOnce(el));
-    wordStats.numberOfPangrams = pangramList.length;
-    wordStats.numAnswers = wordlist.length;
-
-    for (let w of wordlist)
+    for (let w of puzzleData.answers)
     {
         let twoLetterPrefix = w.substring(0, 2).toUpperCase();
         twoLetter2Count[twoLetterPrefix] = (twoLetter2Count[twoLetterPrefix] ?? 0) + 1;
     }
 
-    wordStats.perfectPangramList = perfectPangramList;
-
     return {
-        wordStats,
+        wordStats: {
+            numberOfPangrams: puzzleData.pangrams.length,
+            numAnswers: puzzleData.answers.length,
+            perfectPangramList: puzzleData.pangrams.filter(isEachLetterUsedOnce),
+        },
         twoLetter2Count
     };
 }
 
 
-function buildPrefixCountElement(words, forumInfo)
+function buildPrefixCountElement(words, hintInfo)
 {
     let prefix2Count = {};
     for (let w of words)
@@ -174,21 +103,21 @@ function buildPrefixCountElement(words, forumInfo)
     let $wordStats = document.createElement("div");
     $wordStats.classList.add("sb-extras-wordstats");
     $wordStats.innerHTML = `
-        <span id="_pangramcount">Number of Pangrams: ${forumInfo.wordStats.numberOfPangrams}` +
-        (forumInfo.wordStats.perfectPangramList.length > 0 ?
-            ` <span id="_perfectpangramcount">(${forumInfo.wordStats.perfectPangramList.length} perfect)</span>` :
+        <span id="_pangramcount">Number of Pangrams: ${hintInfo.wordStats.numberOfPangrams}` +
+        (hintInfo.wordStats.perfectPangramList.length > 0 ?
+            ` <span id="_perfectpangramcount">(${hintInfo.wordStats.perfectPangramList.length} perfect)</span>` :
             "") +
         "</span><br>" +
-        `Number of Answers: ${forumInfo.wordStats.numAnswers}`;
+        `Number of Answers: ${hintInfo.wordStats.numAnswers}`;
     $outer.append($wordStats);
 
     let pangramsFound = Array.from(document.querySelectorAll(".sb-wordlist-window .sb-anagram.pangram")).map(el => el.innerText.trim().toLowerCase());
     let perfectPangramsFound = pangramsFound.filter(word => isEachLetterUsedOnce(word));
 
-    if (pangramsFound.length === forumInfo.wordStats.numberOfPangrams)
+    if (pangramsFound.length === hintInfo.wordStats.numberOfPangrams)
         $wordStats.querySelector("#_pangramcount").classList.add("sb-extras-done");
 
-    if (forumInfo.wordStats.perfectPangramList.length > 0 && perfectPangramsFound.length === forumInfo.wordStats.perfectPangramList.length)
+    if (hintInfo.wordStats.perfectPangramList.length > 0 && perfectPangramsFound.length === hintInfo.wordStats.perfectPangramList.length)
         $wordStats.querySelector("#_perfectpangramcount").classList.add("sb-extras-done");
 
 
@@ -206,7 +135,7 @@ function buildPrefixCountElement(words, forumInfo)
     $el.innerText = "# Got";
 
     let $tb = $wrapper.createTBody();
-    let needPairs = Object.keys(forumInfo.twoLetter2Count);
+    let needPairs = Object.keys(hintInfo.twoLetter2Count);
     needPairs.sort();
 
     for (let p of needPairs)
@@ -215,7 +144,7 @@ function buildPrefixCountElement(words, forumInfo)
         $el = $tr.insertCell();
         $el.innerText = p;
         $el = $tr.insertCell();
-        let needCount = forumInfo.twoLetter2Count[p];
+        let needCount = hintInfo.twoLetter2Count[p];
         $el.innerText = needCount;
         $el = $tr.insertCell();
         let gotCount = prefix2Count[p] ?? 0;
@@ -254,17 +183,11 @@ function displayCounts($el)
 }
 
 
-function update(forumInfo)
+function update(hintInfo)
 {
     let words = getFoundWords();
-    let $el = buildPrefixCountElement(words, forumInfo);
+    let $el = buildPrefixCountElement(words, hintInfo);
     displayCounts($el);
-}
-
-
-function getPuzzleData()
-{
-    return unsafeWindow?.gameData?.today ?? {};
 }
 
 
@@ -327,11 +250,8 @@ function styleFoundWords($els)
 
 async function main()
 {
-    let letters = unsafeWindow.gameData?.today?.validLetters;
-    if (!letters)
-        throw Error("failed to get letters for this puzzle: ${isoPuzzleDateStr}");
-
-    let isoPuzzleDateStr = getPuzzleISODate();
+    let puzzleData = requirePuzzleData();
+    let isoPuzzleDateStr = puzzleData.printDate;
 
     GM.registerMenuCommand("Previous puzzle", () => { navTo(isoPuzzleDateStr, -1) });
     GM.registerMenuCommand("Next puzzle", () => { navTo(isoPuzzleDateStr, +1) });
@@ -394,11 +314,14 @@ async function main()
     });
 
     await waiter.match();
-    let forumInfo = await fetchHintInfo(isoPuzzleDateStr, letters);
+    let hintInfo = buildHintInfo(puzzleData);
 
-    update(forumInfo);
+    update(hintInfo);
 
-    foundWordWaiter.match(($els) => styleFoundWords($els));
+    foundWordWaiter.match(($els) => {
+        styleFoundWords($els);
+        update(hintInfo);
+    });
 }
 
 try {
